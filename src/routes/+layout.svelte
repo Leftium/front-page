7ee000935e4c3f6005b5aa87186d488b7d2678bf
Leftium <381217+Leftium@users.noreply.github.com>
2025-10-21 01:37:58 -0500

    feat: add minimal CSS.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,3 +10,13 @@
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	/* based on: https://www.swyx.io/css-100-bytes, https://thecascade.dev/article/least-amount-of-css */
+	:global(html) {
+		max-width: min(70ch, 100% - 1em);
+		margin: auto;
+		line-height: 1.75;
+		font-size: 1.25em;
+	}
+</style>
