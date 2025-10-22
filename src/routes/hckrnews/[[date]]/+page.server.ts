fc220bdd3db8aa9bbae44657edc011c7e815a032
Leftium <381217+Leftium@users.noreply.github.com>
2025-10-22 03:22:02 -0500

    feat: add date-based data fetching via optional route parameter

--- src/routes/hckrnews/[[date]]/+page.server.ts
+++ src/routes/hckrnews/[[date]]/+page.server.ts
@@ -1,7 +1,7 @@
 export const load = async ({ fetch, params }) => {
-	console.log('date param:', params.date);
+	const filename = params.date ? `${params.date.replace(/\./g, '')}.js` : 'latest.js';
 
-	const fetched = await fetch('https://hckrnews.com/data/latest.js');
+	const fetched = await fetch(`https://hckrnews.com/data/${filename}`);
 	const texted = await fetched.text();
 
 	const json = JSON.parse(texted.replace(/^var entries =/, ''));
