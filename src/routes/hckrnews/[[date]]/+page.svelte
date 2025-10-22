5bd6d07a2e89e9af5dd27a20548ee010482efd9e
Leftium <381217+Leftium@users.noreply.github.com>
2025-10-22 04:27:46 -0500

    feat: show ? for comment count on dead items

--- src/routes/hckrnews/[[date]]/+page.svelte
+++ src/routes/hckrnews/[[date]]/+page.svelte
@@ -92,7 +92,7 @@
 						>{points} {@render upvote()}</s-points
 					>
 					<s-comments class:high={comments >= 100} class:mid={comments >= 50 && comments < 100}
-						>{comments} {@render message()}</s-comments
+						>{dead ? '?' : comments} {@render message()}</s-comments
 					>
 					<s-date>{relativeTime(date)}</s-date>
 					{#if showTime}
